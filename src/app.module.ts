70e4eb4526142216725925a207767940616f57c9
unknown <smohammadrahil@gmail.com>
2020-10-12 11:31:42 +0530

    Introspection available

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { BlogModule } from './blog/blog.module';
-import { CommonModule } from './common/common.module';
+import {Module} from '@nestjs/common'
+import {ConfigModule} from '@nestjs/config'
+import {GraphQLModule} from '@nestjs/graphql'
+import {MongooseModule} from '@nestjs/mongoose'
+import {AuthModule} from './auth/auth.module'
+import {BlogModule} from './blog/blog.module'
+import {CommonModule} from './common/common.module'
 
 @Module({
   imports: [
@@ -18,10 +18,11 @@ import { CommonModule } from './common/common.module';
       autoSchemaFile: 'gqls/blog.gql',
       include: [BlogModule],
       path: '/blog',
-      playground : {
-        title : 'Dot Blog Service',
+      introspection: true,
+      playground: {
+        title: 'Dot Blog Service',
       },
-      context: ({ req }) => ({ req }),
+      context: ({req}) => ({req}),
     }),
     AuthModule,
     CommonModule,
